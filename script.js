49d9130163f36b3b023361eae87f42ee612d6d6d
Mar-oz <skodamaros09@gmail.com>
2025-12-03 16:30:30 +0100

    asdjsad

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 const button1 = document.querySelector('#button1');
 
-const url = https://opentdb.com/api.php?amount=10;
+const url = text.link("https://www.w3schools.com");
+
+//text.link("https://www.w3schools.com"/); https://opentdb.com/api.php?amount=10;
