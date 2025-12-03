3f4ce226fea94153d199e3a3511c4f5050408d5b
Mar-oz <skodamaros09@gmail.com>
2025-12-03 17:28:54 +0100

    asdasdas

--- script.js
+++ script.js
@@ -1,5 +1,39 @@
 const button1 = document.querySelector('#button1');
+const url = "https://opentdb.com/api.php?amount=10&category=20&difficulty=medium&type=multiple"
+const hlas = document.querySelector('#hlas')
 
-const url = text.link("https://www.w3schools.com");
+async function pressQuestions (){
 
-//text.link("https://www.w3schools.com"/); https://opentdb.com/api.php?amount=10;
+    if (button1){
+        button1.addEventListener('click', async () => {
+            try {
+                document.getElementById("hlas").textContent = Questions;
+                const response = await fetch (url);
+                const data = await response.json();
+                
+                const Questions  = JSON.stringify(data.results, null, 2);  
+                const correctAnswers = data.results.map(item => item.correct_answer);
+                const incorrectAnswers = data.results.map(item => item.incorrect_answers);
+                console.log(correctAnswers);
+                console.log(incorrectAnswers);
+                console.log(Questions);
+
+            } catch (error) {
+                console.error("Error:", error);
+            }
+        })
+
+}}
+
+    
+pressQuestions();
+
+   /*fetch("https://opentdb.com/api.php?amount=10&category=20&difficulty=medium&type=multiple")
+  .then(response => response.json())
+  .then(data => console.log(data.results))
+  .catch(error => console.error("Error:", error));*/
+  
+
+
+
+//
