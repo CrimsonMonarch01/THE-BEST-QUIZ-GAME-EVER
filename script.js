e1ff5df186aee3ab352a144f539d372c25b8a44f
CrimsonMonarch01 <kakashi0987.senec@gmail.com>
2025-12-11 19:46:59 +0100

    it works but not at it should have

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ const button1 = document.querySelector('#button1');
 const url = "https://opentdb.com/api.php?amount=10&category=20&difficulty=medium&type=multiple"
 const hlas = document.querySelector('#hlas')
 const zobrazenieAsi = document.querySelector('#quiz-container') //ked to bude fungovat tak huraaa 
-const checkAndsend = document.querySelector('#send')
+const send = document.querySelector('#send')
 let correct_answer = ''
 let incorrect_answers = ''
-
+let sprodp = []
 
 async function pressQuestions (){
 
@@ -17,22 +17,23 @@ async function pressQuestions (){
                 const data = await response.json(); // caka na response
                 
                 let vsetko = '' //HTML veci
+                sprodp = [] //
+
 
                 if (!data.results) {
                     console.error("no data")
 
                     return
                 }
-                    
-
+                       
                 
 
-
-                data.results.forEach(ans => {
+                //poslanie dat a zobrazenie v html
+                data.results.forEach((ans, index ) => {
                     vsetko += `
                    <div>
                     <label for=""><name="otazka" class=" otazka"> ${ans.question}</label>
-                    <label for=""><input type="radio" name="otazka"  class=" odpoved"> ${ans.correct_answer}</label>
+                    <label for=""><input type="radio" name="otazka"  class=" odpoved"> ${ans.correct_answer}$</label>
                     <label for=""><input type="radio" name="otazka"  class=" odpoved"> ${ans.incorrect_answers[0]}</label>
                     <label for=""><input type="radio" name="otazka"  class=" odpoved"> ${ans.incorrect_answers[1]}</label>
                     <label for=""><input type="radio" name="otazka"  class=" odpoved"> ${ans.incorrect_answers[2]}</label>
@@ -40,7 +41,7 @@ async function pressQuestions (){
                     
     </div>
                 `
-                
+                sprodp[index] = ans.correct_answer
 
 
                 
@@ -61,20 +62,36 @@ async function pressQuestions (){
 
 }}
 
-function sendIt(){
-    checkAndsend.addEventListener('click', () => {
-        console.log("clicked")
-    })
-    if (checkAndsend === correct_answer){
-        console.log("correct")    
+function sendIt(){ // posleanie odpovedi
+    send.addEventListener('click', () => {
+        let  spravne = 0;        
+      
+        for (let i = 0; i <  sprodp.length;i++){
+            const vybrate = document.querySelector(`input[name="otazka${i}"]:checked`)
+            
+            if (vybrate && vybrate.value === sprodp[1]) {  // for loop na cheknutie nasich otaZOK a plus body ale nejako to mu jebe nevieme preco 
+            spravne++;
+            }
+        }
+        // doplnenie statov v html vo viditelnosti stranky
+        hlas.innerHTML = `
+            <h2>Tvoje skóre: ${spravne} / 10</h2> 
+            <p>Úspešnosť: ${Math.round(spravne / 10 * 100)} %</p>
+        `;
+        // nato nech send neklikne viac krat po sebe 
+        send.disabled = true;
+
+
+
+      //  if (correct_answer === send) {
+          //  spravne++;
+       // } 
+       
 
         
-    }else {
-        (checkAndsend === incorrect_answers)
-            console.log("wrong")
-    }
-return
-
+        
+    })
+    
 }
 
 /*  <label for=""><input type="radio" name="Odpoved"> ${ans.incorrect_answer}</label>
